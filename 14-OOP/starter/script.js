acd193249232c8c0e2a1754c256b0dcc12de7c71
adefenwa <sotundeemmanuel@gmail.com>
2024-05-24 22:22:48 +0100

    latest commit

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -404,31 +404,31 @@
 // jay.calcAge();
 
 // ANOTHER CLASS EXAMPLE
-class Account {
-   constructor(owner, currency, pin) {
-      this.owner = owner;
-      this.currency = currency;
-      this.pin = pin;
-      this.movements = [];
-      this.locale = navigator.language;
+// class Account {
+//    constructor(owner, currency, pin) {
+//       this.owner = owner;
+//       this.currency = currency;
+//       this.pin = pin;
+//       this.movements = [];
+//       this.locale = navigator.language;
 
-      console.log(`Thanks for opening an account, ${owner}`);
-   }
+//       console.log(`Thanks for opening an account, ${owner}`);
+//    }
 
-   // Public interface
-   deposit(val) {
-      this.movements.push(val)
-   }
+//    // Public interface
+//    deposit(val) {
+//       this.movements.push(val)
+//    }
 
-   withdrawal(val) {
-      this.deposit(-val);
-   }
-}
+//    withdrawal(val) {
+//       this.deposit(-val);
+//    }
+// }
 
-const acc1 = new Account('Emmanuel', 'NGN', 1111);
+// const acc1 = new Account('Emmanuel', 'NGN', 1111);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
-acc1.deposit(250);
-acc1.withdrawal(140);
-console.log(acc1);
+// // acc1.movements.push(250);
+// // acc1.movements.push(-140);
+// acc1.deposit(250);
+// acc1.withdrawal(140);
+// console.log(acc1);
