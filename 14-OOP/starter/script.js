07a6be4d4a77441d45507a84edf91619fc73d521
adefenwa <sotundeemmanuel@gmail.com>
2024-05-25 10:26:41 +0100

    latest commit

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -403,33 +403,38 @@
 // jay.introduce();
 // jay.calcAge();
 
-// ANOTHER CLASS EXAMPLE
+// ANOTHER CLASS EXAMPLE / DAY 108 - ENCAPSULATION: PROTECTED PROPERTIES AND METHODS
 class Account {
    constructor(owner, currency, pin) {
       this.owner = owner;
       this.currency = currency;
-      this.pin = pin;
-      this.movements = [];
+      // protected property
+      this._pin = pin;
+      this._movements = [];
       this.locale = navigator.language;
 
       console.log(`Thanks for opening an account, ${owner}`);
    }
 
    // Public interface
+   getMovements(){
+      return this._movements;
+   }
+   
    deposit(val) {
-      this.movements.push(val)
+      this._movements.push(val)
    }
 
    withdrawal(val) {
       this.deposit(-val);
    }
 
-   approveLoan(val) {
+   _approveLoan(val) {
     return true
    }
 
    requestLoan(val) {
-    if(this.approveLoan(val)) {
+    if(this._approveLoan(val)) {
       this.deposit(val);
       console.log('Loan approved!');
     } 
@@ -440,10 +445,11 @@ class Account {
 
 const acc1 = new Account('Emmanuel', 'NGN', 1111);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
 acc1.deposit(250);
 acc1.withdrawal(140);
 acc1.requestLoan(1000)
 acc1.approveLoan();
+console.log(acc1.getMovements());
 console.log(acc1);
