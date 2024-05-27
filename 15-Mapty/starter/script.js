6fadbf5217f4caadcf51f08ae428c2056207b29a
adefenwa <sotundeemmanuel@gmail.com>
2024-05-28 00:20:35 +0100

    latest commit

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -12,3 +12,10 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigagtor.geolocation) 
+    navigator.geolocation.getCurrentPosition(function(position) {
+        console.log(position);
+    }, function() {
+        alert('Could not get your position!')
+    })
