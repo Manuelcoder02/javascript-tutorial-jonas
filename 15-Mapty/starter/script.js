a4b22be3c8774358cb438c065d6785e0c328d50a
adefenwa <sotundeemmanuel@gmail.com>
2024-05-28 00:29:41 +0100

    latest commit

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -17,7 +17,7 @@ if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(function(position) {
         const {latitude} = position.coords;
         const {longitude} = position.coords;
-        console.log(latitude, longitude);
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
     }, function() {
         alert('Could not get your position!')
     })
