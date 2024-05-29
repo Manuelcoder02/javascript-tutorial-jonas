a668cc4fce6fa413f64ad382869e5b6e8a832c8b
adefenwa <sotundeemmanuel@gmail.com>
2024-05-29 02:00:01 +0100

    latest commit

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -19,13 +19,15 @@ if (navigator.geolocation)
         const {longitude} = position.coords;
         console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-        const map = L.map('map').setView([51.505, -0.09], 13);
+        const coords = [latitude, longitude];
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        const map = L.map('map').setView(coords, 13);
+
+L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker(coords).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
     }, function() {
