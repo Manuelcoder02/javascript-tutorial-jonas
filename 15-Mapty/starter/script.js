e59c31b0a2c8c122cd209d85f9aca69d967fd962
adefenwa <sotundeemmanuel@gmail.com>
2024-05-31 02:06:18 +0100

    latest commit

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -22,7 +22,7 @@ if (navigator.geolocation)
 
         const coords = [latitude, longitude];
 
-        const map = L.map('map').setView(coords, 13);
+        map = L.map('map').setView(coords, 13);
 
 L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
