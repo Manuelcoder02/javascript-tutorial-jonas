867cd1e084d5def3c26a821e7779ddb64cbdea37
adefenwa <sotundeemmanuel@gmail.com>
2024-06-04 19:50:13 +0100

    latest commit

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -18,7 +18,7 @@ let map, mapEvent;
 class Workout {
 
     date = new Date();
-    id = (new Date() + '').slice(-10);
+    id = (Date.now() + '').slice(-10);
     constructor(coords, distance, duration) {
         this.coords = coords; // [lat, lng]
         this.distance = distance; // in km
