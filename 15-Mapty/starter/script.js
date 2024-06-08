de534ba28a79c00cdb7800569f23fec64e0e2491
adefenwa <sotundeemmanuel@gmail.com>
2024-06-08 09:10:26 +0100

    latest commit

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -112,6 +112,16 @@ L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         inputDistance.focus();
     }
 
+    _hideForm() {
+        // Empty the strings
+        inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = '';
+        
+        form.style.display = 'none';
+        form.classList.add('hidden');
+        setTimeout(()=> form.style.display = 'grid', 1000)
+
+    }
+
     _toggleElevationField(){
         inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
         inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
@@ -168,7 +178,7 @@ L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         this._renderWorkout(workout);
     
         // Hide input form + clear input fields
-            inputCadence.value = inputDistance.value = inputDuration.value = inputElevation.value = '';
+        this._hideForm();
     
     }
     _renderWorkoutMarker(workout) {
