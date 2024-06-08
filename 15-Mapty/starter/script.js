d457f624afe2876ab6288234ffd31474233ea551
adefenwa <sotundeemmanuel@gmail.com>
2024-06-08 08:42:47 +0100

    latest commit

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -176,7 +176,20 @@ L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
     }
 
     _renderWorkout(workout) {
-
+         const html = `
+         <li class="workout workout--${workout.type}" data-id="${workout.id}">
+          <h2 class="workout__title">${workout.description}</h2>
+          <div class="workout__details">
+            <span class="workout__icon">${workout.type === 'running' ? '🏃‍♂️': '🚴‍♀️'}</span>
+            <span class="workout__value">${workout.distance}</span>
+            <span class="workout__unit">km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⏱</span>
+            <span class="workout__value">${workout.duration}</span>
+            <span class="workout__unit">min</span>
+          </div>
+          </li>`
     }
 }
 
