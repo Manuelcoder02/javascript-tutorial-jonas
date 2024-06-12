e18db2e28cc9c23ba7e7244a9ed832d9346c60c2
adefenwa <sotundeemmanuel@gmail.com>
2024-06-12 12:09:03 +0100

    latest commit

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -9,6 +9,19 @@ request.open('GET', 'https://restcountries.com/v3.1/name/nigeria')
 request.send();
 
 request.addEventListener('load', function(){
-    const data = JSON.parse(this.responseText)
+    const [data] = JSON.parse(this.responseText)
     console.log(data);
+
+    const html = `
+    <article class="country">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${data.population}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages}</p>
+            <p class="country__row"><span>💰</span>${data.currencies}</p>
+          </div>
+        </article>
+    `
 })
