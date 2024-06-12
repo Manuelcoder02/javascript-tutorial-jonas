105be12e3c8029c583b9e407de424be3a507b26b
adefenwa <sotundeemmanuel@gmail.com>
2024-06-12 10:21:04 +0100

    latest commit

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -8,7 +8,7 @@ const request = new XMLHttpRequest();
 request.open('GET', 'https://restcountries.com/v3.1/name/nigeria')
 request.send();
 
-
 request.addEventListener('load', function(){
-    console.log(this.responseText);
+    const data = JSON.parse(this.responseText)
+    console.log(data);
 })
