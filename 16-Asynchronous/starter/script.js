542158cb826223982eac5c6fd266138124199356
adefenwa <sotundeemmanuel@gmail.com>
2024-06-18 22:20:53 +0100

    latest commit

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -42,7 +42,22 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData('ghana')
 // getCountryData('china')
 
-const getCountryData = function(country) {
+const renderCountry = function(data) {
+  const html = `
+      <article class="country">
+            <img class="country__img" src="${data.flags.png}" />
+            <div class="country__data">
+              <h3 class="country__name">${data.name.common}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+              <p class="country__row"><span>🗣️</span>${valueL}</p>
+              <p class="country__row"><span>💰</span>${curr.name}</p>
+            </div>
+          </article>
+      `
+}
+
+const getCountryAndNeighbour = function(country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`)
   request.send();
@@ -59,23 +74,11 @@ const getCountryData = function(country) {
      console.log(curr.name);
       
   
-      const html = `
-      <article class="country">
-            <img class="country__img" src="${data.flags.png}" />
-            <div class="country__data">
-              <h3 class="country__name">${data.name.common}</h3>
-              <h4 class="country__region">${data.region}</h4>
-              <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
-              <p class="country__row"><span>🗣️</span>${valueL}</p>
-              <p class="country__row"><span>💰</span>${curr.name}</p>
-            </div>
-          </article>
-      `
-  
+        
       countriesContainer.insertAdjacentHTML('beforeend', html)
       countriesContainer.style.opacity = 1;
   })
   }
   getCountryData('nigeria')
-  getCountryData('ghana')
-  getCountryData('china')
+  // getCountryData('ghana')
+  // getCountryData('china')
